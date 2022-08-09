df59b405117cb567113cc4d0a25d8007407c8f2b
demershow <demersontorres520@gmail.com>
2022-08-09 11:57:19 -0300

    aula 91: i did delete method

--- routes/users.js
+++ routes/users.js
@@ -1,39 +1,45 @@
 let NeDB = require('nedb');
 
 let db = new NeDB({
-    filename:'users.db',
-    autoload: true,
-})
+    filename: 'users.db',
+    autoload: true
+});
 
 
-module.exports = app =>{
+module.exports = app => {
 
-    app.get('/users', (req, res) => {
+    let route = app.route('/users')
 
-        res.statusCode = 200;
-        res.setHeader('Content-type', 'application/json');
-        res.json({
-            users: [{
-                name: 'Demerson',
-                email: 'demersontorres520@gmail.com',
-                id: 'oi',
+    route.get((req, res) => {
 
-            }]
-        });
+        db.find({}).sort({ name: 1 }).exec((err, users) => {
+
+            if (err) {
+                app.utils.error.send(err, req, res);
+            } else {
+
+                res.statusCode = 200;
+                res.setHeader('Content-type', 'application/json');
+                res.json({
+                    users
+
+                });
+
+            }
+
+        })
+
+
 
     });
 
-    app.post('/users', (req, res) => {
+    route.post((req, res) => {
 
-        db.insert(req.body, (err, user) =>{
+        db.insert(req.body, (err, user) => {
 
-            if(err){
-                console.log('error: ${err}');
-                res.status(400).json({
-                    error: err
-                });
-
-            }else{
+            if (err) {
+                app.utils.error.send(err, req, res);
+            } else {
 
                 res.status(200).json(user);
             }
@@ -41,6 +47,50 @@ module.exports = app =>{
         })
 
     })
+
+    let routeId = app.route('/users/:id');
+
+    routeId.get((req, res) => {
+
+        db.findOne({ _id: req.params.id }).exec((err, user) => {
+
+            if (err) {
+                app.utils.error.send(err, req, res);
+            } else {
+                res.status(200).json(user);
+            }
+
+        });
+
+    });
+
+    routeId.put((req, res) => {
+
+        db.update({ _id: req.params.id }, req.body, err => {
+
+            if (err) {
+                app.utils.error.send(err, req, res);
+            } else {
+                res.status(200).json(Object.assign(req.params, req.body));
+            }
+
+        });
+
+    });
+
+    routeId.delete((req, res) => {
+
+        db.remove({ _id: req.params.id }, {}, err => {
+
+            if (err) {
+                app.utils.error.send(err, req, res);
+            } else {
+                res.status(200).json(req.params);
+            }
+
+        });
+
+    });
 }
 
 
